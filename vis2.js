7ce41097a804ec2948f0f1210d80d4ae2e2f4ad8
jhf5167 <64052767+jhf5167@users.noreply.github.com>
2022-10-17 16:20:15 +1100

    1

--- vis2.js
+++ vis2.js
@@ -1,2 +1,2 @@
-var vega = 'vis2.vg.json';
+var vega = 'https://raw.githubusercontent.com/jfoo0014/jfoo0014.github.io/main/vis2.vg.json';
 vegaEmbed('#chart1', vega).then(function(result){}).catch(console.error);

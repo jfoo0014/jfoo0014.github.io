20f88d353d370821b242dd864954dbf6ce78db8e
jhf5167 <64052767+jhf5167@users.noreply.github.com>
2022-10-18 21:39:19 +1100

    8

--- temp.py
+++ temp.py
@@ -3,5 +3,5 @@ net = pd.read_csv('share-of-individuals-using-the-internet.csv')
 life = pd.read_csv('life-expectancy.csv', skiprows=3)
 life = pd.melt(life, 'Country Code', [str(x) for x in range(1990,2018)])
 life['variable'] = [int(x) for x in life['variable']]
-res = pd.merge(net, life, how='inner', left_on=['Code','Year'], right_on=['Country Code','variable'])[['Entity','Code','Year','Individuals using the Internet (% of population)','value']].rename(columns={'Individuals using the Internet (% of population)': 'Internet Usage', 'value': 'Life Expectancy'})
+res = pd.merge(net, life, how='inner', left_on=['Code','Year'], right_on=['Country Code','variable'])[['Entity','Code','Year','Individuals using the Internet (% of population)','value']].rename(columns={'Individuals using the Internet (% of population)': 'InternetUsage', 'value': 'LifeExpectancy'})
 res.to_csv('datavis2.csv')

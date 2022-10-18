332d51ab19f6f39a13a2ea60518330f480a3dc67
jhf5167 <64052767+jhf5167@users.noreply.github.com>
2022-10-18 19:33:05 +1100

    3

--- vis2.js
+++ vis2.js
@@ -1,2 +1,2 @@
-var vega = 'https://raw.githubusercontent.com/jfoo0014/jfoo0014.github.io/main/vis2.vg.json';
-vegaEmbed('#chart1', vega).then(function(result){}).catch(console.error);
+var vega1 = 'https://raw.githubusercontent.com/jfoo0014/jfoo0014.github.io/main/vis21.vg.json';
+vegaEmbed('#chart1', vega1).then(function(result){}).catch(console.error);
